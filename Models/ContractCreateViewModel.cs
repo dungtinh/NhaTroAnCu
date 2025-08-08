7e4c164aea3debf4c881e8aafbeae05bfdea3122
dungtinh <tindd.hd@gmail.com>
2025-08-08 21:03:24 +0700

    Thêm thuộc tính đặt cọc và thu cọc ngay vào ContractCreateViewModel

--- Models/ContractCreateViewModel.cs
+++ Models/ContractCreateViewModel.cs
@@ -1,41 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace NhaTroAnCu.Models
+public class ContractCreateViewModel
 {
-    public class ContractCreateViewModel
-    {
-        public int RoomId { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime MoveInDate { get; set; }
-        public int Months { get; set; }
-        public decimal PriceAgreed { get; set; }
-        public decimal DepositAmount { get; set; }
-        public string Note { get; set; }
+    // ... các thuộc tính khác ...
 
-        public decimal ElectricityPrice { get; set; }
-        public decimal WaterPrice { get; set; }
+    public decimal DepositAmount { get; set; }
 
-        // Deposit collection options
-        public bool CollectDepositNow { get; set; }
-        public decimal? DepositCollectionAmount { get; set; }
-
-        // Danh sách người thuê
-        public List<TenantInputModel> Tenants { get; set; } = new List<TenantInputModel>();
-    }
-
-    public class TenantInputModel
-    {
-        public string FullName { get; set; }
-        public string IdentityCard { get; set; }
-        public string PhoneNumber { get; set; }
-        public DateTime? BirthDate { get; set; }
-        public string Gender { get; set; }
-        public string PermanentAddress { get; set; }
-        public string Ethnicity { get; set; }        // Thêm mới
-        public string VehiclePlate { get; set; }     // Thêm mới
-    }
-
+    // Thu cọc ngay khi tạo hợp đồng
+    public bool CollectDepositNow { get; set; }
+    public decimal? ActualDepositCollected { get; set; }
 }
