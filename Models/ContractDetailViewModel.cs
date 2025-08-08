80fe44985994b01d429654104dba6e8ddc4f25a7
dungtinh <tindd.hd@gmail.com>
2025-08-08 21:03:30 +0700

    Bổ sung thuộc tính trạng thái thu cọc vào ContractDetailViewModel

---
+++ Models/ContractDetailViewModel.cs
@@ -0,0 +1,8 @@
+public class ContractDetailViewModel
+{
+    // ... các thuộc tính khác ...
+
+    public decimal DepositAmount { get; set; }
+    public bool DepositCollected { get; set; }
+    public decimal DepositCollectedAmount { get; set; }
+}
